bafe1d7fd04e30de432cbe925296259e2c8eff15
Aadi Rajan <rajan.adhyatma@gmail.com>
2026-05-19 11:50:32 -0700

    initialize sdss manager

--- sdss-local-manager/sdss_manager/__init__.py
+++ sdss-local-manager/sdss_manager/__init__.py
@@ -1,1 +1,13 @@
+# sdss_manager/__init__.py
 
+# Expose the main classes
+from .downloader import SDSSDownloader
+from .indexer import SDSSIndexer
+from .core import SDSSDatabase
+
+# Package metadata
+__version__ = "0.1.0"
+__author__ = "Adhyatma Rajan"
+
+# Control what gets imported with "from sdss_manager import *"
+__all__ = ["SDSSDownloader", "SDSSIndexer", "SDSSDatabase"]
